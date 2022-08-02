2631156802f1a5dab3e54d5dd6169b3527060f91
quanta2015 <liyangtom@163.com>
2022-08-02 10:30:54 +0800

    添加 less

--- auto.sh
+++ auto.sh
@@ -2,19 +2,24 @@ DIRECTORY='scripts'
 
 
 npx husky install
-npx husky add .husky/pre-commit "node scripts/preCommit.js" 
-npx husky add .husky/post-commit "git push && npm publish" 
+
+
+npx husky add .husky/pre-commit "exec 1>&2" 
+npx husky add .husky/pre-commit "exec < /dev/tty" 
+npx husky add .husky/pre-commit "npx eslint --fix --ext .js,.jsx,.ts,.tsx src" 
+npx husky add .husky/pre-commit "npx stylelint 'src/**/*.less' --syntax less" 
+npx husky add .husky/pre-commit "npm run prettier" 
+npx husky add .husky/pre-commit "node script/preCommit.js" 
+npx husky add .husky/pre-commit "git add ." 
+
+
+npx husky add .husky/post-commit "git push && npm run build && npm publish" 
+
 git clone git@github.com:quanta2015/AutoProc.git
 
 if [ -d "$DIRECTORY" ]; then
-  cp ./AutoProc/*.js ./scripts
+  cp ./AutoProc/preCommit.js ./scripts
   rm -rf ./AutoProc
 else
   mv ./AutoProc ./scripts
 fi
-
-
-
-npm version major 结果0.0.1->1.0.0
-npm version minor 结果0.0.1->0.1.0
-npm version patch 结果0.0.1->0.0.2
--- preCommit.js
+++ preCommit.js
@@ -1,52 +1,51 @@
-/*----------------------
-// 1. call eslint
-// 2. run prettier
-// 3. add version + 1
-// 4. npm run build
-// 5. publish
-------------------------*/
+const fs = require('fs');
+const { AutoComplete } = require('enquirer');
 
-const {cmd, version} = require("./fn")
+const CrReset  = "\x1b[0m"
+const FgYellow = "\x1b[33m"
+const FgWhite  = "\x1b[1m"
+const PKG_FILE     = `package.json`
+const debug    =(e)=> console.log(`${FgYellow}${e}${CrReset}`)
+const succ     =(e)=> console.log(`${FgWhite}${e}${CrReset}`)
 
-const format       =(e)=> `\n${LINE}${e}\n${LINE}`
-const LINE         = `------------------\n`
-const INFO_COMMIT  = format('请先提交代码!')
-const INFO_ESLINT  = format('检查代码规范...')
-const INFO_PRETTEY = format('格式化代码中...')
-const INFO_VERSION = format('修改工程版本!')
-const INFO_BUILD   = format('编译代码中...')
-const INFO_PUSH    = format('提交代码中...')
-const CMD_ESLINT   = `npx eslint --fix --ext .js,.jsx,.ts,.tsx src`
-const CMD_PRETTEY  = `npx prettier --write src/**/*.{js,jsx,tsx,ts,less,md,json}`
-const CMD_ADDFILE  = `git add .`
-const CMD_BUILD    = `npm run build`
-const CMD_PUSH     = `npm publish`
+
+const getVersion = new AutoComplete({
+  message: '要升级哪个版本?\n',
+  choices: [
+    'patch',
+    'minor',
+    'major',
+  ]
+});
+
+const chgVer = async(index)=> {
+  const data = fs.readFileSync(PKG_FILE,{encoding:'utf8', flag:'r'})
+  const pkg = JSON.parse(data)
+  const ver = pkg.version
+  const arr = ver.split('.')
+  const len = arr.length-1
+
+  arr[index] = `${parseInt(arr[index])+1}`
+  pkg.version = arr.join('.')
+  const file = JSON.stringify(pkg, null, 4)
+  fs.writeFileSync(PKG_FILE, file)
+
+  debug(`version: ${pkg.version}\n`)
+  succ('successful!')
+}
+
+const version = async()=> {
+  const ver = await getVersion.run();
+
+  switch(ver) {
+    case 'major': chgVer(0);break;
+    case 'minor': chgVer(1);break;
+    case 'patch': chgVer(2);break;
+  }
+}
 
 const run =async()=>{
-  // 1. 检查代码格式
-  await cmd(CMD_ESLINT, INFO_ESLINT)
-
-  // 2. 格式化代码
-  await cmd(CMD_PRETTEY,INFO_PRETTEY)
-
-  // 3. 添加版本号
-  version()
-
-  // 4. 执行 git add .
-  await cmd(CMD_ADDFILE, null)
-
-  // 5. 编译代码
-  await cmd(CMD_BUILD,INFO_BUILD)
-
-  // 检查代码是否提交
-  // const clean = await isClean()
-  // if (!clean) {
-  //   console.log(INFO_COMMIT)
-  //   process.exit(1);
-  // }
-
-  // // 提交项目
-  // await cmd(CMD_PUSH,INFO_PUSH)
+  await version()
 }
 
 run()
